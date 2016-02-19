3189fcadab0cb8d8dc06f2fa2207f7e265de716c
Aladdin <alaaden3000@gmail.com>
2016-02-19 03:25:57 +0200

    #4

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,21 @@
 "use strict";
 var app = angular.module('hsApp', []);
 app.controller('hsCtrl',['$scope', '$http', function($scope, $http){
-  $scope.itemTemplate = {
-    structureName: null,
-    structure:[
-    {
-      name: null,
-      guiType: null,
-      validation:[null]
-    }]
-  };
-  $scope.add = function(){
-    $scope.itemTemplateNew = $scope.itemTemplate;
+  // $scope.itemTemplate = {
+  //   structureName: null,
+  //   structure:[
+  //   {
+  //     name: null,
+  //     guiType: null,
+  //     validation:[null]
+  //   }]
+  // };
+  $scope.counter = 0;
+  $scope.itemTemplate = {};
+  $scope.itemTemplate.structure = [];
+  $scope.add = function(itemTemplate){
+    $scope.itemTemplate.structure.push(itemTemplate.structure); 
     console.log($scope.itemTemplate);
+    $scope.counter += 1 ;
   }
 }]);
